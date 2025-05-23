e2a5c0c3c3b5f1f85e96dd96a0940e89e77750ff
AnDev58 <andev58@outlook.com>
2025-05-23 13:57:06 +0300

    Refactor

--- static/js/keyboard.js
+++ static/js/keyboard.js
@@ -3,43 +3,51 @@ import Movement from "./movement.js";
 export default class KeyboardMovement extends Movement {
   
   enable() {
-    document.body.addEventListener("keydown", this.#onKeyDown)
-    document.body.addEventListener("keyup", this.#onKeyUp)
+    document.body.addEventListener("keydown", this.onKeyDown)
+    document.body.addEventListener("keyup", this.onKeyUp)
   }
 
   reset() {
-    document.body.removeEventListener("keydown", this.#onKeyDown)
-    document.body.removeEventListener("keyup", this.#onKeyUp)
+    document.body.removeEventListener("keydown", this.onKeyDown)
+    document.body.removeEventListener("keyup", this.onKeyUp)
     super.reset()
   }
 
-  #onKeyDown(ev) { this.#changeKeys(ev.key.toLowerCase(), 1) }
-  #onKeyUp(ev) { this.#changeKeys(ev.key.toLowerCase(), 0) }
+  onKeyDown = (ev) => { this.#changeKeys(ev.key.toLowerCase(), 1) }
+  onKeyUp = (ev) => { this.#changeKeys(ev.key.toLowerCase(), 0) }
 
   #changeKeys(key, value) {
     switch(key) {
         case "shift":
-            this.moveHeight = value;
+            this.moveHeight = -value;
             break;
         case " ":
-            this.moveHeight = -value;
+            this.moveHeight = value;
             break;
         case "w":
             this.moveX = value;
+            break;
         case "s":
             this.moveX = -value;
+            break;
         case "a":
-            this.moveY = -value;
-        case "d":
             this.moveY = value;
+            break;
+        case "d":
+            this.moveY = -value;
+            break;
         case "arrowleft":
             this.moveAngle = -value;
+            break;
         case "arrowright":
             this.moveAngle = value;
+            break;
         case "arrowup":
             this.movePitch = value;
+            break;
         case "arrowdown":
             this.movePitch = -value;
+            break;
     }
 }
 }

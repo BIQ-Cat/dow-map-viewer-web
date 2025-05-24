b48505fb48a142e54ac5a7dd2de4337eadb30046
AnDev58 <andev58@outlook.com>
2025-05-24 10:18:29 +0300

    merged

---
+++ static/js/elements.js
@@ -0,0 +1,13 @@
+export const canvas = document.querySelector("canvas");
+export const ctx = canvas.getContext("2d");
+
+export const joystickContainer = document.querySelector(".joystick");
+export const joystickHandle = document.querySelector(".joystick-handle");
+
+export const swipeStartElement = canvas;
+
+export const upButton = document.getElementById("up")
+export const downButton = document.getElementById("down")
+export const heightController = document.querySelector(".height-controller")
+
+export const settings = document.querySelector(".canvas-controller")
